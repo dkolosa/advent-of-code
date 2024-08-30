8a34420a78c0af0f80b9e3a5fa05f018d24e0f2c
Daniel Kolosa <contact@dkolosa.com>
2024-08-30 16:26:04 -0700

    Added files to gitignore Added moved part1 to function

--- 2020/day_1/src/main.rs
+++ 2020/day_1/src/main.rs
@@ -1,18 +1,7 @@
 
 use std::{fs, str::FromStr};
 
-fn main() {
-
-    let file_path = "input_p1.txt";
-    let target = 2020;
-    let input_txt = fs::read_to_string(file_path).expect("Cannot read file");
-
-    let values: Vec<&str> = input_txt.split("\n").collect();
-    let mut int_values: Vec<i32> = Vec::new();
-
-    for str_vales in values.iter().enumerate(){
-        int_values.push(i32::from_str(str_vales.1).unwrap());
-    }
+fn part1(mut int_values:Vec<i32>, target:i32) {
 
     let mut r: usize = 0;
     let mut l: usize = 1;
@@ -33,5 +22,21 @@ fn main() {
         }
 
     }
+}
+
+fn main() {
+
+    let file_path = "input_p1.txt";
+    let target = 2020;
+    let input_txt = fs::read_to_string(file_path).expect("Cannot read file");
+
+    let values: Vec<&str> = input_txt.split("\n").collect();
+    let mut int_values: Vec<i32> = Vec::new();
+
+    for str_vales in values.iter().enumerate(){
+        int_values.push(i32::from_str(str_vales.1).unwrap());
+    }
+
+    part1(int_values, target);
   
 }
