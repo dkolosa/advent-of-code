f4df7a07b7f2201e2bf48ec2c49c4221da1cadd8
Dan <dan@MacBook-Air.local>
2024-09-17 16:58:17 -0700

    completed Day 18 part 1

--- 2020/day_18/src/main.rs
+++ 2020/day_18/src/main.rs
@@ -3,47 +3,54 @@ use std::fs;
 
 fn main() {
 
-    let filename = "input.txt";
+    let filename = "input_p1.txt";
 
     let contents: Vec<String> = fs::read_to_string(filename).expect("file not found")
                             .lines()
                             .map(|s| s.to_string())
                             .collect();
 
+    let mut final_sum:i64 = 0;
     
     for line in contents {
 
-        // You will have two stacks (number stack and operator stack)
-        // one result variable
-        // Push on the stack for ( and pop on the stack for ) until hit a matching (
-        // 
         // let mut result = 0;
         let mut op: char = '_';
-
-        let mut prev_value = 0;
+        let mut prev_value:i64 = 0;
 
         let mut idx = 0;
+
         let char_line: Vec<char> = line.chars().collect();
-
         while idx < char_line.len() {
-            let cn = char_line[idx];
-
-            if cn.is_digit(10) {
-                math_op(cn, op, &mut prev_value);
-            } else if cn == '+' || cn =='*' {
-                op = cn;
-            } else if cn == '(' { }
-            idx += 1;
+            parse_chars(&char_line, &mut idx, &mut op, &mut prev_value);
         }
-        println!("{}", prev_value)
-
+        final_sum += prev_value;
+        println!("{}", prev_value);
     }
-    
-    
+    println!("Final Sum: {}", final_sum);
 }
 
-fn math_op(cn: char, op: char, prev_value: &mut i32) {
-    let dig = cn.to_digit(10).unwrap() as i32;
+fn parse_chars(char_line: &Vec<char>, idx: &mut usize, op: &mut char, prev_value: &mut i64) {
+    while *idx < char_line.len(){
+        let cn = char_line[*idx];
+        *idx += 1;
+        if cn == ')' {
+            return;
+        }
+        if cn.is_digit(10) {
+            let dig = cn.to_digit(10).unwrap() as i64;
+            math_op(dig, *op, prev_value);
+        } else if cn == '+' || cn =='*' {
+            *op = cn;
+        } else if cn == '(' { 
+            let mut inner_value = 0;
+            parse_chars(char_line, idx,&mut '_', &mut inner_value);
+            math_op(inner_value, *op, prev_value);
+        }
+    }
+}
+
+fn math_op(dig:i64, op: char, prev_value: &mut i64) {
     match op {
         '+' => *prev_value += dig,
         '*' => *prev_value *= dig,
