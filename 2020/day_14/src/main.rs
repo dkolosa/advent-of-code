2d5d22c54d3f389397155d1596d3d245cec5f4e7
Daniel Kolosa <contact@dkolosa.com>
2024-09-26 17:17:56 -0700

    Added masking operations

--- 2020/day_14/src/main.rs
+++ 2020/day_14/src/main.rs
@@ -1,9 +1,12 @@
+use std::string;
+
 struct B36(u64);
 impl B36 {
 
     fn new(value :u64)-> Self {
         Self(value & ((1u64 << 36))- 1)
     }
+
     fn set_bit(&mut self, position: usize) {
         if position < 36 {
             self.0 |= 1 << position;
@@ -24,18 +27,43 @@ impl B36 {
         }
     }
     
-    fn get_value(&self) -> u64 {
+    fn value(&self) -> u64 {
         self.0
     }
 
-    fn apply_mask(&self, mask: u64) -> u64{
-        
-        let and_mask: u64 = 0b111;
-        let or_mask: u64 = 0b111;
+    fn mask_to_or(mask:&String) -> u64{
+
+        let mask_str:String = mask.chars()
+                        .map(|c| {if c == 'X' {'1'} else {'0'}})
+                        .collect();
+        let binary_mask = u64::from_str_radix(&mask_str, 2).expect("Invalid value");
+
+        // Ensure it is 36-bits
+        binary_mask & ((1u64 << 36))- 1
+                        
+    }
+
+    fn mask_to_and(mask:&String) -> u64 {
+
+        let mask_str: String = mask.clone().chars()
+                        .map(|c| {if c == 'X' {'0'} else {'1'}})
+                        .collect();
+        let binary_mask = u64::from_str_radix(&mask_str, 2).expect("Invalid value");
+
+        // Ensure it is 36-bits
+        binary_mask & ((1u64 << 36))- 1
+    }
+
+
+    fn apply_mask(&self, mask: &String) -> u64{
+ 
+        let and_mask: u64 = Self::mask_to_and(mask);
+
+        let or_mask: u64 = Self::mask_to_or(mask);
 
         (self.0 & and_mask) | or_mask
-
     }
+
 
 
 }
@@ -43,12 +71,17 @@ impl B36 {
 fn main() {
 
 
-    let mut bit: B36 = B36::new(0b1011);
-    bit.set_bit(20);
-    println!("The bit value {}", bit.get_value());
+    let bit: B36 = B36::new(101);
+    let mask:String = String::from("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+    let masked_bit  = bit.apply_mask(&mask);
 
-    bit.clear_bit(20);
-    println!("Bit cleared {}", bit.get_value());
-    
-    println!("is bit 12 set {}", bit.check_bit_set(12));
+    let mut sum = 0;
+
+    if B36::mask_to_and(&mask) ^ masked_bit == 0 {
+        //Take the value
+        sum += masked_bit;
+    }
+
+    println!("Total sum: {}", sum);
+
 }
